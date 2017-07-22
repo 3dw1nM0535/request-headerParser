a1c991e616b08a58bc4795cc71222cab841a7837
Edwin Moses <workockmoses@gmail.com>
2017-07-22 10:22:24 +0300

    header parse

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var os = require('os');
+var parser = require('ua-parser-js');
 
 var app = express();
 
 app.get('/', (req, res) => {
+  var osName = parser(req.get('user-agent')).os.name;
+  var osVersion = parser(req.get('user-agent')).os.version;
   var hostDetails = {
     "ipaddress": req.ip,
-    "Software": os.arch() + " " + os.type(),
+    "Software": osVersion + ' ' + osName,
+    "language": req.get('accept-language').split(',')[0],
   }
   res.json(hostDetails);
-  console.log(req.headers);
 });
 
 app.listen(3000, '127.0.0.1', () => {
